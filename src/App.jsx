342c3e6324e347bc7f85637161feebed45a867f2
Munna-Scripts <munnascriptz@gmail.com>
2025-06-29 17:24:11 +0600

    React Router Setup Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
 import './App.css'
-import { Navbar } from './Component/Navbar'
 import { LayoutOne } from './Layout/LayoutOne'
+import { Dashboard } from './Pages/Dashboard'
+import { About } from './Pages/About'
+import { Blog } from './Pages/Blog'
+import { Login } from './Pages/Login'
+import { Register } from './Pages/Register'
 
 function App() {
   const MyRoute = createBrowserRouter(createRoutesFromElements(
     <Route>
       <Route path='/' element={<LayoutOne/>}>
-        
+        <Route index element={<Dashboard/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Blog' element={<Blog/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Resister' element={<Register/>}/>
       </Route>
     </Route>
 
   ))
   return (
     <>
-      <Navbar></Navbar>
+    <RouterProvider router={MyRoute} />
     </>
   )
 }
--- src/Layout/LayoutOne.jsx
+++ src/Layout/LayoutOne.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router'
+import { Navbar } from '../Component/Navbar'
 
 export const LayoutOne = () => {
   return (
-    <div>LayoutOne</div>
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
   )
 }
