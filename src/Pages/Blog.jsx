13ab7f25dad4d9bf0697e5d32377be4c9fcac017
Munna-Scripts <munnascriptz@gmail.com>
2025-07-03 18:09:47 +0600

    Blog Page Started

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { FaRegUser } from "react-icons/fa";
+import { ImFilesEmpty } from "react-icons/im";
+import { CiCalendarDate } from "react-icons/ci";
 
 export const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+      <section>
+        <div className='mb-5'>
+          <div><img className='w-[600px]' src="src\assets\Blog.png" alt="image" /></div>
+        </div>
+        <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-1.5 text-[14px]'><FaRegUser /><p className='font-Inter text-Primary '>By Admin</p></div>
+          <div className='flex items-center gap-1.5 text-[14px]'><ImFilesEmpty /><p className='font-Inter text-Primary '>Category</p></div>
+          <div className='flex items-center gap-1.5 text-[14px]'><CiCalendarDate /><p className='font-Inter text-Primary '>August 19, 2025</p></div>
+          
+
+        </div>
+      </section>
+    </>
   )
 }
