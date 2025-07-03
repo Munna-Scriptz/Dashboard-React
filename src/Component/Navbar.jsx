30679b03a61628d7813f53a7663dd2ada34b128b
Munna-Scripts <munnascriptz@gmail.com>
2025-07-03 18:36:09 +0600

    Navbar Scroll Problem Fixed

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -38,8 +38,8 @@ export const Navbar = () => {
   ]
 
   return (
-    <>
-      <nav className='SideBar bg-white py-5 px-5 w-[250px] h-screen flex flex-col relative'>
+    <div className='relative w-[320px] h-screen'>
+      <nav className='SideBar bg-white py-5 px-5 w-[250px] h-screen flex flex-col absolute fixed'>
         <div className='absolute w-full h-40 left-0 top-0 z-0 bg-[url(./assets/Shape1.png)] bg-center bg-no-repeat bg-cover'></div>
         <div className="flex flex-col items-center z-10 mb-6">
           <div><img className='w-40 rounded-full border-3 border-[#0099ff8e] outline-3 outline-white' src="src\assets\Zangetsu.jpg" alt="Profile" /></div>
@@ -64,6 +64,6 @@ export const Navbar = () => {
         </div>
         <div className='absolute w-full h-20 left-0 bottom-0 z-0 bg-[url(./assets/Shape2.png)] bg-center bg-no-repeat bg-cover'></div>
       </nav>
-    </>
+    </div>
   )
 }
