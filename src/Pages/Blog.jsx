f02365e8003e5dc39731aad6bdb0a7e76d5c8f09
Munna-Scripts <munnascriptz@gmail.com>
2025-07-03 19:28:42 +0600

    Blog Search Box Completed

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { FaRegUser } from "react-icons/fa";
 import { ImFilesEmpty } from "react-icons/im";
 import { CiCalendarDate } from "react-icons/ci";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export const Blog = () => {
   return (
     <>
-      <section>
+      <section className='flex justify-between pr-4'>
+        {/* --------Left Side-------- */}
         <div className="Left_Side">
           <div className='mb-5'>
-            <div><img className='w-[700px]' src="src\assets\Blog.png" alt="image" /></div>
+            <div><img className='w-[650px]' src="src\assets\Blog.png" alt="image" /></div>
           </div>
           <div className='flex items-center gap-3'>
             <div className='flex items-center gap-1.5 text-[15px]'><FaRegUser /><p className='font-Inter text-Primary '>By Admin</p></div>
@@ -17,7 +19,7 @@ export const Blog = () => {
             <div className='flex items-center gap-1.5 text-[15px]'><CiCalendarDate /><p className='font-Inter text-Primary '>August 19, 2025</p></div>
           </div>
           <div>
-            <h1 className=" font-Inter font-bold text-[32px] w-[700px] text-[#192335] my-2.5">
+            <h1 className=" font-Inter font-bold text-[32px] w-[650px] text-[#192335] my-2.5">
               Exploring the Impact of Education: A Comprehensive Guide
             </h1>
             <p className="w-[700px] font-Inter text-[#192335] text-[14px]">
@@ -27,7 +29,7 @@ export const Blog = () => {
               Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem IpsumContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock
             </p>
             {/* ----Quotes---- */}
-            <div className='bg-[#F3F6FB] p-10 mt-5 '>
+            <div className='bg-[#F3F6FB] p-6 mt-5 '>
               <div  className='flex gap-7 mb-5'>
                 <div><img className='w-[47px]' src="src\assets\Quote.png" alt="image" /></div>
                 <div>
@@ -40,10 +42,30 @@ export const Blog = () => {
                   </div>
                 </div>
               </div>
-              
+            </div>
+            <p className="w-[650px] font-Inter text-[#192335] text-[14px] mt-5">
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virgini 
+            </p>
+          </div>
+        </div>
+        {/* --------Right Side-------- */}
+        <div className='Right_Side'>
+          <div className='w-[340px] p-[20px] border-2 border-[#1923352c] rounded-[7px]'>
+            <div>
+              <h1 className=" font-Inter font-bold text-[22px] text-[#192335] my-2.5 mt-0">
+                Search Here
+              </h1>
+              <div className='relative bg-[#E2E5F1] w-full h-[3px]'>
+                <div className='absolute bg-[#2F57EF] w-30 h-full'></div>
+              </div>
+            </div>
+            <div className='mt-5 relative'>
+             <input placeholder="Search..." className="input border-2 border-gray-200 px-7 py-2.5 rounded-xl outline-none w-full" name="search" type="search"/>
+              <FaMagnifyingGlass className="absolute top-3 right-3 size-5" color='#2F57EF'/>
             </div>
           </div>
         </div>
+
       </section>
     </>
   )
